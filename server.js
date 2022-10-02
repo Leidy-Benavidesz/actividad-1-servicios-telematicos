47f52b2ab9672867ac851f6d8204503139b59db1
Leidy-Benavides <leybenavidesz@outlook.com>
2022-10-01 19:39:59 -0500

    puerto cambiado

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(5500, () => {
+app.listen(process.env.PORT, () => {
     console.log('Express server started at port : 3000');
 });
 
